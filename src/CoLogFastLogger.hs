3787ec20927ae3a7c9e6a32fda9485d1ca69dd94
chessai <chessai1996@gmail.com>
2019-10-15 13:12:37 -0400

    Creō ā nihilō

--- src/CoLogFastLogger.hs
+++ src/CoLogFastLogger.hs
@@ -1,6 +1,28 @@
 module CoLogFastLogger
-       ( someFunc
-       ) where
+  ( withFastLogAction
 
-someFunc :: IO ()
-someFunc = putStrLn ("someFunc" :: String)
+    -- * Re-exports
+    -- ** From co-log
+  , module CologCore
+
+    -- ** From fast-logger
+  , module SystemLogFastLogger
+  ) where
+
+import System.Log.FastLogger as SystemLogFastLogger
+import Colog.Core as CologCore
+import Prelude hiding (log)
+import Control.Monad.IO.Class (MonadIO(..))
+import qualified Control.Monad.Catch as Exceptions
+
+type FastLogAction m = LogAction m LogStr
+
+withFastLogAction :: (MonadIO m, Exceptions.MonadMask m)
+  => LogType
+  -> (FastLogAction m -> m a)
+  -> m a
+withFastLogAction typ f = Exceptions.bracket
+  (liftIO (newFastLogger typ))
+  (liftIO . snd)
+  (f . (\g -> LogAction (liftIO . g)) . fst)
+{-# specialise withFastLogAction :: LogType -> (FastLogAction IO -> IO a) -> IO a #-}
